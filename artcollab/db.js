d81253dd44bb81b0dfb8ebbdd33c778616efcf11
Greeshma Shettigar <shettigargreeshma@gmail.com>
2025-07-05 14:04:26 +0530

    Final changes before deployment

--- artcollab/db.js
+++ artcollab/db.js
@@ -1,14 +1,18 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbUri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/coartistry', {
+    await mongoose.connect(dbUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
     process.exit(1);
   }
 };
