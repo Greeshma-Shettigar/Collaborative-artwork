93ded332d43bf13ea7e4acb8432afb28e792dd65
Greeshma Shettigar <shettigargreeshma@gmail.com>
2025-07-09 01:53:12 +0530

    fixing Tools.jsx

--- client/src/Tools.jsx
+++ client/src/Tools.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { getColorPalette } from './utils/colorAPI';
-import { socket } from "../socket"; 
+import socket from "./socket"; // default export
+
 
 import {
   FaPencilAlt,
